884dc3580b0cd28fe027468f1822185816cbb863
Waleed Mustafa Qureshi <129791489+WaleedQureshi141@users.noreply.github.com>
2025-02-17 12:14:56 -0500

    setup 1

---
+++ profile-backend/src/main/java/com/waleed/profile/profile_backend/ProfileBackendApplication.java
@@ -0,0 +1,21 @@
+package com.waleed.profile.profile_backend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileBackendApplication {
+
+	public static void main(String[] args) 
+	{
+		Dotenv dotenv = Dotenv.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
+				entry.getValue()));
+
+		SpringApplication.run(ProfileBackendApplication.class, args);
+	}
+
+}
