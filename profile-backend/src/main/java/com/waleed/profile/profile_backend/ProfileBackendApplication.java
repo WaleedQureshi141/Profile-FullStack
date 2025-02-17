c69913acfdb3205ed0cc08b58ac1256898fb1f86
Waleed Mustafa Qureshi <129791489+WaleedQureshi141@users.noreply.github.com>
2025-02-17 14:42:41 -0500

    rds connected

--- profile-backend/src/main/java/com/waleed/profile/profile_backend/ProfileBackendApplication.java
+++ profile-backend/src/main/java/com/waleed/profile/profile_backend/ProfileBackendApplication.java
@@ -10,10 +10,28 @@ public class ProfileBackendApplication {
 
 	public static void main(String[] args) 
 	{
-		Dotenv dotenv = Dotenv.load();
+		// this way should work if the .env file is in the root directory
+		// it did not work for me so I provided the complete path to .env file
 
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
-				entry.getValue()));
+		// Dotenv dotenv = Dotenv.load();
+
+		// dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
+		// 		entry.getValue()));
+
+		// using the complete .env path
+		try 
+		{
+			Dotenv dotenv = Dotenv.configure().directory("D:/Personal Projects/Profile-Page/Profile-FullStack/profile-backend/.env").load();
+
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
+                    entry.getValue()));
+
+            System.out.println("Environment variables loaded successfully.");
+        } 
+		catch (Exception e) 
+		{
+            System.err.println("Error loading .env file: " + e.getMessage());
+        }
 
 		SpringApplication.run(ProfileBackendApplication.class, args);
 	}
